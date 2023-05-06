55c9ffc6c2c9f6021750449f4e6813cf2e0469c9
Replit user <>
2023-05-06 23:29:22 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -105,6 +105,17 @@ List* get_adj_nodes(Node* n){
 
 
 int is_final(Node* n){
+  int i , j , num;
+
+    for(i = 0 ; i < 9 ; i++){
+      for(j = 0 ; j < 9 ; j++){
+        if(n->sudo[i][j] == 0)
+        {
+          return 0;
+        }
+      }
+    }
+  
     return 0;
 }
 
